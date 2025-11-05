d3e97eec0e6cd9a5b7b276419d4f969a850675b1
jivikajain90-sketch <jivikajain90@gmail.com>
2025-11-05 20:40:30 +1100

    Update app.py

--- app.py
+++ app.py
@@ -418,7 +418,7 @@ app.layout = html.Div([
                    href='https://qilt.edu.au/docs/default-source/default-document-library/2024-gos-international-report.pdf?sfvrsn=168c5da_1',
                    target='_blank',
                    style={'fontSize': '16px', 'color': '#002E79', 'textDecoration': 'none', 'fontWeight': '600'})
-        ], className='source-box', style={'width': '68%', 'marginLeft': '13', 'marginTop': '-12px', 
+        ], className='source-box', style={'width': '68%', 'marginLeft': '13px', 'marginTop': '-32px', 
                                           'padding': '2px', 'textAlign': 'center'})
     ]),
     
